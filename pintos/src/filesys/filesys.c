abc8e35fc81020223304ca85881c916046780483
Masihbr <masihbr@gmail.com>
2023-05-23 22:07:53 +0330

    Flush cache in filesys_done

--- pintos/src/filesys/filesys.c
+++ pintos/src/filesys/filesys.c
@@ -38,8 +38,9 @@ void
 filesys_done (void)
 {
   free_map_close ();
+  cache_flush ();
 }
-
+
 /* Creates a file named NAME with the given INITIAL_SIZE.
    Returns true if successful, false otherwise.
    Fails if a file named NAME already exists,
