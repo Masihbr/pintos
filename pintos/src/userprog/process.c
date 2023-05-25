ea038be345d5904ac04d2f21a5f014e14e8ab396
Masihbr <masihbr@gmail.com>
2023-05-25 13:08:15 +0330

    Fix start_process so process won't start without wait

--- pintos/src/userprog/process.c
+++ pintos/src/userprog/process.c
@@ -94,8 +94,8 @@ start_process (void *file_name_)
      arguments on the stack in the form of a `struct intr_frame',
      we just point the stack pointer (%esp) to our stack frame
      and jump to it. */
-  asm volatile ("movl %0, %%esp; jmp intr_exit" : : "g" (&if_) : "memory");
   sema_up (&thread_current ()->sema);
+  asm volatile ("movl %0, %%esp; jmp intr_exit" : : "g" (&if_) : "memory");
   NOT_REACHED ();
 }
 
